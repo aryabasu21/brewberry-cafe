01e7c7fbcebd498b52805b2dfe8b975fd5ab4aa6
Arya Basu <basu.arya007@gmail.com>
2025-02-02 20:04:23 +0530

    new change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,9 +8,9 @@ function Home() {
         <div className="content">
           <h1>Welcome to Brewberry Cafe</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            scelerisque, nisl nec luctus lacinia, libero lacus imperdiet odio,
-            id aliquet justo libero sit amet justo. Donec ac urna nec nisl
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
+            reiciendis impedit laborum quibusdam nihil beatae distinctio ratione
+            eos voluptatibus sed.
           </p>
         </div>
       </div>
